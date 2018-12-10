d559de594a294b5951d14a91e53b653707ae025a
phamhungsonk60 <sonph@kaopiz.com>
2018-12-10 13:25:37 +0700

    create route-view-controller

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PostController@home')->name('home');
 
+//Route::get('/', 'PostController@home');
+Route::resource('/posts','PostController');
